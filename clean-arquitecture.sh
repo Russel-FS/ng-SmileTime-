4e8f8e6bd85be038d11adefb86b8fff74b8f3bb5
russel <russelfloressolano900@gmail.com>
2025-03-02 17:16:51 +0000

    tests

--- clean-arquitecture.sh
+++ clean-arquitecture.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
+ 
+# Estructura de carpetas principal adaptada a Clean Architecture
 
-# estructura de carpetas principal
-mkdir -p src/app/core/{domain,use-cases,interfaces,services}
-mkdir -p src/app/data/{repositories,datasources,models}
+# Capa de dominio: 
+mkdir -p src/app/core/{domain/models,use-cases,interfaces,services}
+
+# Capa de datos:  
+mkdir -p src/app/data/{repositories,datasources}
+
+# Capa de presentación
 mkdir -p src/app/presentation/{pages,components,shared}
 mkdir -p src/app/presentation/shared/layout/{components,containers}
+
+# Infraestructura
 mkdir -p src/app/infrastructure/{http,storage,config}
+
+# Recursos compartidos
 mkdir -p src/app/shared/{constants,utils,types}
 
-
-# archivos .gitkeep 
+# Archivos .gitkeep para mantener la estructura en el repositorio
 touch src/app/core/domain/.gitkeep
+touch src/app/core/domain/domain-models/.gitkeep
 touch src/app/core/use-cases/.gitkeep
 touch src/app/core/interfaces/.gitkeep
 touch src/app/core/services/.gitkeep
 touch src/app/data/repositories/.gitkeep
 touch src/app/data/datasources/.gitkeep
-touch src/app/data/models/.gitkeep
 touch src/app/presentation/shared/layout/components/.gitkeep
 touch src/app/presentation/shared/layout/containers/.gitkeep
 touch src/app/presentation/pages/.gitkeep
@@ -28,6 +37,7 @@ touch src/app/infrastructure/config/.gitkeep
 touch src/app/shared/constants/.gitkeep
 touch src/app/shared/utils/.gitkeep
 touch src/app/shared/types/.gitkeep
+
 
 
 # permisos de ejecución al script
