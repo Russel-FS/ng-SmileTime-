06dd8846b7e5a09f2fb97ab867c2121380c5d6dc
russel <russelfloressolano900@gmail.com>
2025-03-03 16:58:23 +0000

    personalization footer adn modify header

--- src/app/presentation/shared/layout/components/footer/footer.component.ts
+++ src/app/presentation/shared/layout/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  @Output() navigate = new EventEmitter<string>();
+  onNavigate(route: string) {
+    this.navigate.emit(route);
+  }
 }
