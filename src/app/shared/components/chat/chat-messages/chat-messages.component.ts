3c12dd9772ae6db359199714a304b63f4a0c3156
russel <russelfloressolano900@gmail.com>
2025-03-04 03:19:00 +0000

    I'm adding component children, side bar, header, messages for chat component

---
+++ src/app/shared/components/chat/chat-messages/chat-messages.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-messages',
+  imports: [],
+  templateUrl: './chat-messages.component.html',
+  styleUrl: './chat-messages.component.css'
+})
+export class ChatMessagesComponent {
+
+}
