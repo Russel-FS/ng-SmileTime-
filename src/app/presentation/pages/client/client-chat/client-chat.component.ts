de44301e39db36da1b5f287c3f8a69c42213327e
russel <russelfloressolano900@gmail.com>
2025-03-04 18:19:51 +0000

    I'm delegating the work of Component Chat to child components

--- src/app/presentation/pages/client/client-chat/client-chat.component.ts
+++ src/app/presentation/pages/client/client-chat/client-chat.component.ts
@@ -1,39 +1,30 @@
-import { Component, ViewChild, ElementRef, AfterViewChecked, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { trigger, transition, style, animate } from '@angular/animations';
 import { ChatSidebarComponent } from '../../../../shared/components/chat/chat-sidebar/chat-sidebar.component';
 import { ContactMessage } from '../../../../core/domain/models/contact-message';
-
-interface Message {
-  text: string;
-  isUser: boolean;
-  timestamp: Date;
-}
+import { Message } from '../../../../core/domain/models/messages';
+import { ChatMessagesComponent } from '../../../../shared/components/chat/chat-messages/chat-messages.component';
+import { ChatHeaderComponent } from '../../../../shared/components/chat/chat-header/chat-header.component';
 
 @Component({
   selector: 'app-client-chat',
   standalone: true,
-  imports: [CommonModule, FormsModule, ChatSidebarComponent],
-  templateUrl: './client-chat.component.html',
-  styleUrl: './client-chat.component.css',
-  animations: [
-    trigger('fadeSlide', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'translateY(10px)' }),
-        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
-      ]),
-    ]),
+  imports: [
+    CommonModule,
+    FormsModule,
+    ChatSidebarComponent,
+    ChatMessagesComponent,
+    ChatHeaderComponent,
   ],
+  templateUrl: './client-chat.component.html',
+  styleUrls: ['./client-chat.component.css'],
 })
-export class ClientChatComponent implements AfterViewChecked {
-  @ViewChild('messageContainer') private messageContainer!: ElementRef;
-
+export class ClientChatComponent {
+  contactSelected!: ContactMessage;
   messages: Message[] = [
     { text: 'Hola Dr. Flores Solano, ¿en que puedo ayudar?', isUser: false, timestamp: new Date() },
   ];
-  newMessage: string = '';
-  searchQuery: string = '';
   isTyping: boolean = false;
 
   contacts: ContactMessage[] = [
@@ -43,7 +34,7 @@ export class ClientChatComponent implements AfterViewChecked {
       role: 'Dentista',
       avatar: 'icons/user-profile.svg',
       status: 'online',
-      lastMessage: 'Your appointment is confirmed',
+      lastMessage: 'ultima vez, 10:00',
       unread: 2,
       isActive: true,
     },
@@ -53,7 +44,7 @@ export class ClientChatComponent implements AfterViewChecked {
       role: 'Dentista',
       avatar: 'icons/user-profile.svg',
       status: 'offline',
-      lastMessage: 'Please check your treatment plan',
+      lastMessage: 'ultima vez, 10:00',
       unread: 0,
     },
     {
@@ -62,50 +53,30 @@ export class ClientChatComponent implements AfterViewChecked {
       role: 'soperte',
       avatar: 'icons/user-profile.svg',
       status: 'online',
-      lastMessage: 'Your records are ready',
+      lastMessage: 'ultima vez, 10:00',
       unread: 1,
     },
   ];
-  onScroll(): void {
-    this.checkScrollPosition();
-  }
-
-  ngAfterViewChecked() {
-    this.scrollToBottom();
-  }
-
-  private scrollToBottom(): void {
-    try {
-      this.messageContainer.nativeElement.scrollTop =
-        this.messageContainer.nativeElement.scrollHeight;
-    } catch (err) {}
-  }
 
   onContactClick(contact: ContactMessage) {
     this.contacts.forEach((c) => (c.isActive = false));
     contact.isActive = true;
     contact.unread = 0;
+    this.contactSelected = contact;
   }
 
-  private checkScrollPosition(): void {
-    const container = this.messageContainer.nativeElement;
-    const isNearBottom =
-      container.scrollHeight - container.scrollTop - container.clientHeight < 100;
-  }
-
-  sendMessage() {
-    if (this.newMessage.trim()) {
+  onSendMessage(newMessage: string): void {
+    if (newMessage.trim()) {
       // marcar como escribiendo
       const activeContact = this.contacts.find((c) => c.isActive);
       if (activeContact) activeContact.status = 'typing';
 
       // agregar mensaje
       this.messages.push({
-        text: this.newMessage,
+        text: newMessage,
         isUser: true,
         timestamp: new Date(),
       });
-      this.newMessage = '';
 
       // simular respuesta
       setTimeout(() => {
--- src/app/shared/components/chat/chat-header/chat-header.component.ts
+++ src/app/shared/components/chat/chat-header/chat-header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ContactMessage } from '../../../../core/domain/models/contact-message';
 
 @Component({
   selector: 'app-chat-header',
   imports: [],
   templateUrl: './chat-header.component.html',
-  styleUrl: './chat-header.component.css'
+  styleUrl: './chat-header.component.css',
 })
 export class ChatHeaderComponent {
-
+  @Input() contact!: ContactMessage;
 }
