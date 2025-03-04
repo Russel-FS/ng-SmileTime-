7d32283f3bf81ae81b8853867749bbdf74fd1fa5
russel <russelfloressolano900@gmail.com>
2025-03-04 05:45:01 +0000

    new changes in chat client component

--- src/app/shared/components/chat/chat-sidebar/chat-sidebar.component.ts
+++ src/app/shared/components/chat/chat-sidebar/chat-sidebar.component.ts
@@ -1,12 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ContactMessage } from '../../../../core/domain/models/contact-message';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-chat-sidebar',
   imports: [FormsModule],
   templateUrl: './chat-sidebar.component.html',
   styleUrl: './chat-sidebar.component.css',
+  animations: [
+    trigger('fadeSlide', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, transform: 'translateY(0)' }),
+        animate('300ms ease-out', style({ opacity: 0, transform: 'translateY(10px)' })),
+      ]),
+    ]),
+  ],
 })
 export class ChatSidebarComponent {
   searchQuery: string = '';
