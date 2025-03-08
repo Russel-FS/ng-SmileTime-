f8eda6f66de64b32d565d4915f69df8c237b3648
Sahel-pl <u00030737@sise.com.pe>
2025-03-07 20:53:55 -0500

    Add Changes  for the dashboard and new cosas for  search (filtres)

--- src/app/presentation/components/search/search.component.ts
+++ src/app/presentation/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,13 +7,61 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
-  searchTerm: string = '';
-  @Output() searched = new EventEmitter<string>();
+export class SearchComponent implements OnInit {
+  @Output() searchChange = new EventEmitter<any>();
 
-  onSearch() {
-    this.searched.emit(this.searchTerm);
+  searchTerm: string = '';
+  showFilters: boolean = false;
+  selectedSpecialty: string = '';
+  selectedLocation: string = '';
+  selectedAvailability: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  toggleFilters(): void {
+    this.showFilters = !this.showFilters;
+  }
+
+  onSearch(): void {
+    const searchData = {
+      term: this.searchTerm,
+      filters: {
+        specialty: this.selectedSpecialty,
+        location: this.selectedLocation,
+        availability: this.selectedAvailability
+      }
+    };
+    this.searchChange.emit(searchData);
+  }
+
+  hasActiveFilters(): boolean {
+    return this.selectedSpecialty !== '' || 
+           this.selectedLocation !== '' || 
+           this.selectedAvailability !== '';
+  }
+
+  get activeFilters(): string[] {
+    const filters: string[] = [];
+    if (this.selectedSpecialty) filters.push(this.selectedSpecialty);
+    if (this.selectedLocation) filters.push(this.selectedLocation);
+    if (this.selectedAvailability) filters.push(this.selectedAvailability);
+    return filters;
+  }
+
+  removeFilter(filter: string): void {
+    if (this.selectedSpecialty === filter) {
+      this.selectedSpecialty = '';
+    }
+    if (this.selectedLocation === filter) {
+      this.selectedLocation = '';
+    }
+    if (this.selectedAvailability === filter) {
+      this.selectedAvailability = '';
+    }
+    this.onSearch();
   }
 }
