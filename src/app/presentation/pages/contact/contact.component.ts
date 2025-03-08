6232ebbbd53ccd23fb7b0b13653c1d5fd1382578
Guillermo-lt <u00029926@sise.com.pe>
2025-03-08 12:47:54 -0500

    mejomaiento de form

--- src/app/presentation/pages/contact/contact.component.ts
+++ src/app/presentation/pages/contact/contact.component.ts
@@ -1,12 +1,13 @@
+
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // ✅ Asegura que FormsModule está importado
-
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true, // Si es standalone, este debe estar presente
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css'],
-  imports: [FormsModule] // ✅ Agregar FormsModule aquí si es standalone
+  imports: [FormsModule, CommonModule] // ✅ Agregar FormsModule aquí si es standalone
 })
 export class ContactComponent {
   formData = {
@@ -16,10 +17,20 @@ export class ContactComponent {
     company: '',
     countryCode: '',
     phone: '',
-    language: ''
+    language: '',
+    dentalProblem:'',
+    date:''
   };
+ 
+  
 
+
+  showModal = false;
   submitForm() {
     console.log('Form Data:', this.formData);
+    this.showModal = true;
+  }
+  closeModal() {
+    this.showModal = false; // Cierra el modal cuando se hace clic en el botón
   }
 }
