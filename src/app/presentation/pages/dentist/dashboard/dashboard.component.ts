6a9f3b18dd6bb8065765e0578c7346ebb24696b1
Sahel-pl <u00030737@sise.com.pe>
2025-03-15 01:41:08 -0500

    actu

--- src/app/presentation/pages/dentist/dashboard/dashboard.component.ts
+++ src/app/presentation/pages/dentist/dashboard/dashboard.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../infrastructure/datasources/auth.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  public numero: number = 5456465;
+
 
 }
