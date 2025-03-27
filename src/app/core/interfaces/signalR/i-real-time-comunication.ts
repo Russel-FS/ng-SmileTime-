5f016e0adcc713565d02669429a95ca8f8f005a1
Russel-FS <russelfloressolano900@gmail.com>
2025-03-27 01:59:46 +0000

    feat: actualizar la interfaz IRealTimeComunication y el caso de uso ManageTypingStatus para utilizar TypingStatus

--- src/app/core/interfaces/signalR/i-real-time-comunication.ts
+++ src/app/core/interfaces/signalR/i-real-time-comunication.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { InjectionToken } from '@angular/core';
 import { MessageEntity } from '../../domain/entities/chat/message-entity';
 import { ConversationEntity } from '../../domain/entities/chat/conversation-entity';
+import { TypingStatus } from '../../domain/model/TypingStatus';
 
 export interface IRealTimeComunication {
   connect(): void;
@@ -11,8 +12,8 @@ export interface IRealTimeComunication {
   sendMessage(message: MessageEntity): void;
 
   //estado de typing
-  onTypingStatus(): Observable<{ userId: string | number; isTyping: boolean }>;
-  setTypingStatus(userId: string | number, isTyping: boolean): void;
+  onTypingStatus(): Observable<TypingStatus>;
+  setTypingStatus(typingStatus: TypingStatus): void;
 }
 export const IRealTimeComunication = new InjectionToken<IRealTimeComunication>(
   'RealTimeComunication',
