c109596f1059115d11c0afdc5c0f2adcbb4eff44
Guillermo-lt <u00029926@sise.com.pe>
2025-03-27 02:57:38 -0500

    cambio de servicio

--- src/app/infrastructure/datasources/contact/contact.service.spec.ts
+++ src/app/infrastructure/datasources/contact/contact.service.spec.ts
@@ -1,16 +1,18 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // Importa HttpClientTestingModule
 import { ContactService } from './contact.service';
 
 describe('ContactService', () => {
   let service: ContactService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ContactService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule] // Importa HttpClientTestingModule para pruebas de HttpClient
+    });
+    service = TestBed.inject(ContactService); // Inyecta el servicio ContactService
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica si el servicio fue creado exitosamente
   });
 });
--- src/app/presentation/pages/contact/contact.component.ts
+++ src/app/presentation/pages/contact/contact.component.ts
@@ -24,6 +24,7 @@ export class ContactComponent {
   };
 
   showModal = false;
+  
 
   constructor(private contactService: ContactService) { }
 
