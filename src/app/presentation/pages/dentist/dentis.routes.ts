0d675e7dc059ba0c1f01a5c0a2314c8070736612
Russel-Fs <russelfloressolano900@gmail.com>
2025-03-31 00:13:18 +0000

    feat: reemplazar componente de programación de citas y eliminar el antiguo

--- src/app/presentation/pages/dentist/dentis.routes.ts
+++ src/app/presentation/pages/dentist/dentis.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PatientsComponent } from './patients/patients.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ScheduleComponent } from './schedule/schedule.component';
 import { DentistChatComponent } from './dentist-chat/dentist-chat.component';
+import { AppointmentSchedulingComponent } from './appointment-scheduling/appointment-scheduling.component';
 
 export const dentistRoutes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'patients', component: PatientsComponent },
   { path: 'profile', component: ProfileComponent },
-  { path: 'schedule', component: ScheduleComponent },
+  { path: 'appointment-scheduling', component: AppointmentSchedulingComponent },
   { path: 'chat', component: DentistChatComponent },
 ];
