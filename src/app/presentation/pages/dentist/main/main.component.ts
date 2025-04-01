45dd9b0ccd3afd68392c5825aa83f41cf3b1caef
Russel-Fs <russelfloressolano900@gmail.com>
2025-04-01 19:36:24 +0000

    feat: agregar ruta principal y sidebar de dentista en el componente principal

--- src/app/presentation/pages/dentist/main/main.component.ts
+++ src/app/presentation/pages/dentist/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatSidebarComponent } from "../../../components/chat/chat-sidebar/chat-sidebar.component";
 import { CalendarComponent } from "../../client/appointments/calendar/calendar.component";
+import { DentistSidebarComponent } from "../../../components/dentist/dentist-sidebar/dentist-sidebar.component";
 
 @Component({
   selector: 'app-main',
-  imports: [ChatSidebarComponent, CalendarComponent],
+  imports: [ChatSidebarComponent, CalendarComponent, DentistSidebarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
