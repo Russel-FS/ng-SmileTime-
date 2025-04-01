35c05fbf68b9a28dc79212270ea42b4dc28d4eaf
Russel-Fs <russelfloressolano900@gmail.com>
2025-04-01 19:41:21 +0000

    feat: implementar sidebar de dentista con navegación y estilos mejorados

--- src/app/presentation/components/dentist/dentist-sidebar/dentist-sidebar.component.ts
+++ src/app/presentation/components/dentist/dentist-sidebar/dentist-sidebar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dentist-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './dentist-sidebar.component.html',
   styleUrl: './dentist-sidebar.component.css'
 })
 export class DentistSidebarComponent {
-
+  nuevoPaciente() {
+    // Implementar la lógica para crear nuevo paciente
+    console.log('Crear nuevo paciente');
+  }
 }
