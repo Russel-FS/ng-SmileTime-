aaf0d60b39e622290436904c82f2e9872180f65a
Russel-Fs <russelfloressolano900@gmail.com>
2025-04-02 15:10:22 +0000

    feat: actualizar las rutas del dentista para utilizar loadComponent en lugar de component

--- src/app/presentation/pages/dentist/dentis.routes.ts
+++ src/app/presentation/pages/dentist/dentis.routes.ts
@@ -1,24 +1,43 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { PatientsComponent } from './patients/patients.component';
-import { ProfileComponent } from './profile/profile.component';
-import { DentistChatComponent } from './dentist-chat/dentist-chat.component';
-import { AppointmentSchedulingComponent } from './appointment-scheduling/appointment-scheduling.component';
-import { CalendarComponent } from './calendar/calendar.component';
 
 export const dentistRoutes: Routes = [
   {
-    path: 'main', loadComponent: () => import('./main/main.component').then(m => m.MainComponent),
+    path: 'main',
+    loadComponent: () => import('./main/main.component').then(m => m.MainComponent),
     children: [
-      { path: 'home', loadComponent: () => import('./dentist-home/dentist-home.component').then(m => m.DentistHomeComponent), },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'patients', component: PatientsComponent },
-      { path: 'profile', component: ProfileComponent },
-      { path: 'appointment-scheduling', component: AppointmentSchedulingComponent },
-      { path: 'calendar', component: CalendarComponent },
-      { path: 'chat', component: DentistChatComponent },
+      {
+        path: 'home',
+        loadComponent: () => import('./dentist-home/dentist-home.component').then(m => m.DentistHomeComponent)
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
+      },
+      {
+        path: 'patients',
+        loadComponent: () => import('./patients/patients.component').then(m => m.PatientsComponent)
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent)
+      },
+      {
+        path: 'appointment-scheduling',
+        loadComponent: () => import('./appointment-scheduling/appointment-scheduling.component').then(m => m.AppointmentSchedulingComponent)
+      },
+      {
+        path: 'calendar',
+        loadComponent: () => import('./calendar/calendar.component').then(m => m.CalendarComponent)
+      },
+      {
+        path: 'chat',
+        loadComponent: () => import('./dentist-chat/dentist-chat.component').then(m => m.DentistChatComponent)
+      },
     ]
   },
-  { path: 'calendar', component: CalendarComponent },
+  {
+    path: 'calendar',
+    loadComponent: () => import('./calendar/calendar.component').then(m => m.CalendarComponent)
+  },
   { path: '**', redirectTo: 'main/home', pathMatch: 'full' },
 ];
