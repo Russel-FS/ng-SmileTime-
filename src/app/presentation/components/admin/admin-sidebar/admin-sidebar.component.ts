227c6906bdc97fac197869cd0c1929cc3edd9113
Russel-FS <russelfloressolano900@gmail.com>
2025-04-03 01:31:43 +0000

    feat: agregar componente 'AdminSidebar' con su plantilla y estilos, y actualizar rutas de administración

--- src/app/presentation/components/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/presentation/components/admin/admin-sidebar/admin-sidebar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-sidebar',
-  imports: [],
   templateUrl: './admin-sidebar.component.html',
-  styleUrl: './admin-sidebar.component.css'
+  styleUrls: ['./admin-sidebar.component.css'],
+  standalone: true,
+  imports: [RouterModule]
 })
 export class AdminSidebarComponent {
 
--- src/app/presentation/pages/admin/routes.ts
+++ src/app/presentation/pages/admin/routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 export const adminRoutes: Routes = [
     {
         path: '',
         component: MainComponent,
-        children: []
+        children: [
+            {
+                path: 'home', component: AdminHomeComponent
+            },
+            {
+                path: 'dentistas', component: AdminPanelComponent,
+            }
+        ]
+    }
+    , {
+        path: '**',
+        redirectTo: 'home', pathMatch: 'full'
     }
 ];
