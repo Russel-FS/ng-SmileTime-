474faf7cf691707b89d07cda031ba4be1452865f
Russel-FS <russelfloressolano900@gmail.com>
2025-04-03 01:37:45 +0000

    feat: actualizar diseño del componente 'Main' para incluir 'AdminSidebar' y mejorar la estructura de la página

--- src/app/presentation/pages/admin/main/main.component.ts
+++ src/app/presentation/pages/admin/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ChatSidebarComponent } from "../../../components/chat/chat-sidebar/chat-sidebar.component";
 import { AdminSidebarComponent } from "../../../components/admin/admin-sidebar/admin-sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
-  imports: [AdminSidebarComponent],
+  imports: [AdminSidebarComponent, RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
