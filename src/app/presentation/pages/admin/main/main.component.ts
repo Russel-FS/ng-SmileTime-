4add8e784d2e34df3488594d29892b463b6b0fcc
Russel-FS <russelfloressolano900@gmail.com>
2025-04-03 01:22:03 +0000

    feat: reemplazar componente 'ChatSidebar' por 'AdminSidebar' en 'Main'

--- src/app/presentation/pages/admin/main/main.component.ts
+++ src/app/presentation/pages/admin/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ChatSidebarComponent } from "../../../components/chat/chat-sidebar/chat-sidebar.component";
+import { AdminSidebarComponent } from "../../../components/admin/admin-sidebar/admin-sidebar.component";
 
 @Component({
   selector: 'app-main',
-  imports: [ChatSidebarComponent],
+  imports: [AdminSidebarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
